c45acd127593b10de03d35b85c7bc8dc80b981bf
mg.torres <mg.torres@uniandes.edu.co>
2020-03-23 16:19:16 -0500

    Implementación clases SQL

---
+++ src/main/java/uniandes/isis2304/parranderos/persistencia/SQLServiciosHotel.java
@@ -0,0 +1,88 @@
+package uniandes.isis2304.parranderos.persistencia;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+import uniandes.isis2304.parranderos.negocio.Bar;
+
+public class SQLServiciosHotel 
+{
+
+	/* ****************************************************************
+	 * 			Constantes
+	 *****************************************************************/
+	/**
+	 * Cadena que representa el tipo de consulta que se va a realizar en las sentencias de acceso a la base de datos
+	 * Se renombra acá para facilitar la escritura de las sentencias
+	 */
+	private final static String SQL = PersistenciaIter.SQL;
+	
+	/* ****************************************************************
+	 * 			Atributos
+	 *****************************************************************/
+	/**
+	 * El manejador de persistencia general de la aplicación
+	 */
+	private PersistenciaIter pp;
+	
+	/* ****************************************************************
+	 * 			Métodos
+	 *****************************************************************/
+
+	/**
+	 * Constructor
+	 * @param pp - El Manejador de persistencia de la aplicación
+	 */
+	public SQLServiciosHotel(PersistenciaIter pp)
+	{
+		this.pp = pp;
+	}
+	
+	/**
+	 * Crea y ejecuta la sentencia SQL para adicionar un BAR a la base de datos de Parranderos
+	 * @param pm - El manejador de persistencia
+	 * @param idBar - El identificador del bar
+	 * @param nombre - El nombre del bar
+	 * @param ciudad - La ciudad del bar
+	 * @param presupuesto - El presupuesto del bar (ALTO, MEDIO, BAJO)
+	 * @param sedes - El número de sedes del bar
+	 * @return El número de tuplas insertadas
+	 */
+	public long adicionarServiciosHotel (PersistenceManager pm, long id, long restaurante, long piscina, long parqueadero, long wifi, long tv, long h24 ) 
+	{
+        Query q = pm.newQuery(SQL, "INSERT INTO " + pp.darTablaBar () + "(id, nombre, ciudad, presupuesto, cantsedes) values (?, ?, ?, ?, ?)");
+        q.setParameters(id,restaurante, piscina, parqueadero, wifi, tv,h24);
+        return (long) q.executeUnique();
+	}
+
+
+	/**
+	 * Crea y ejecuta la sentencia SQL para eliminar UN BAR de la base de datos de Parranderos, por su identificador
+	 * @param pm - El manejador de persistencia
+	 * @param idBar - El identificador del bar
+	 * @return EL número de tuplas eliminadas
+	 */
+	public long eliminarServiciosHotelPorId (PersistenceManager pm, long id)
+	{
+        Query q = pm.newQuery(SQL, "DELETE FROM " + pp.darTablaBar () + " WHERE id = ?");
+        q.setParameters(id);
+        return (long) q.executeUnique();
+	}
+
+	/**
+	 * Crea y ejecuta la sentencia SQL para encontrar la información de UN BAR de la 
+	 * base de datos de Parranderos, por su identificador
+	 * @param pm - El manejador de persistencia
+	 * @param idBar - El identificador del bar
+	 * @return El objeto BAR que tiene el identificador dado
+	 */
+	public Bar darServiciosHotelPorId (PersistenceManager pm, long id) 
+	{
+		Query q = pm.newQuery(SQL, "SELECT * FROM " + pp.darTablaBar () + " WHERE id = ?");
+		q.setResultClass(Bar.class);
+		q.setParameters(id);
+		return (Bar) q.executeUnique();
+	}
+	
+
+}
